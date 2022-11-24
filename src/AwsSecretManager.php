255ee1fcc3bacb9a1c5f2ca152f45426639f8b11
Mohd Rizwan <rizwan@involve.asia>
2022-11-24 09:46:05 +0800

    initial release

--- src/AwsSecretManager.php
+++ src/AwsSecretManager.php
@@ -2,7 +2,87 @@
 
 namespace Mhdriz1\AwsSecretManager;
 
+use Aws\Exception\AwsException;
+use Aws\SecretsManager\SecretsManagerClient;
+
 class AwsSecretManager
 {
-    // Build your next great package.
+    /**
+     * @return array
+     */
+    public static function get(string $values): array
+    {
+        try {
+
+            $args = [
+                'SecretId'     => $values,
+            ];
+
+            $result = self::getClient()->getSecretValue($args);
+
+            return json_decode($result->get('SecretString'), true);
+
+        }  catch (AwsException $e) {
+
+        }
+
+        return [];
+    }
+
+    public static function all(): array
+    {
+        $result = [];
+
+        foreach (self::getAllList() as $secret) {
+            $result[$secret] = self::get($secret);
+        }
+
+        return $result;
+    }
+
+    private static function getAllList() : array
+    {
+        $secretList = [];
+        $nextToken  = null;
+
+        try {
+
+            while(true) {
+                $args = [];
+
+                if($nextToken) {
+                    $args['NextToken'] = $nextToken;
+                }
+
+                $result = self::getClient()->listSecrets($args);
+
+                foreach ($result->get('SecretList') as $secret) {
+                    $secretList[] = $secret['Name'];
+                }
+
+                $nextToken    = $result->get('NextToken');
+
+                if($nextToken == null) {
+                    break;
+                }
+            }
+
+        }  catch (AwsException $e) {
+
+        }
+
+        return $secretList;
+    }
+
+    /**
+     * @return SecretsManagerClient
+     */
+    public static function getClient(): SecretsManagerClient
+    {
+        return new SecretsManagerClient([
+            'profile' => config('secret-manager.profile'),
+            'version' => config('secret-manager.version'),
+            'region'  => config('secret-manager.region'),
+        ]);
+    }
 }
